906e4156b34eab9afb0b045841d50dea65478bcf
garrieta.ext <gerardojao@gmail.com>
2026-06-04 12:29:27 +0200

    env prod

--- src/Components/support.js
+++ src/Components/support.js
@@ -1,5 +1,5 @@
 export const SUPPORT_EMAIL =
-  import.meta.env.VITE_SUPPORT_EMAIL || "gerardojao@gmail.com";
+  import.meta.env.VITE_SUPPORT_EMAIL || "zagaprosystem@gmail.com";
 
 export function supportMailto(subject = "Soporte ZagaPro") {
   return `mailto:${SUPPORT_EMAIL}?subject=${encodeURIComponent(subject)}`;
